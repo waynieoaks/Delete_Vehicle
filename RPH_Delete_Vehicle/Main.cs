adfa03c0f65e2229cca7d7d24b17b0d73b744553
Waynieoaks <5962977+waynieoaks@users.noreply.github.com>
2022-09-10 10:49:23 +0100

    Initial template
    Initial template to configure

---
+++ RPH_Delete_Vehicle/Main.cs
@@ -0,0 +1,143 @@
+﻿using Rage;
+using Rage.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPH_Delete_Vehicle
+{
+    public static class EntryPoint
+
+    {
+        public static string INIpath = "Plugins\\RPH_Delete_Vehicle.ini";
+        public static Keys ExampleKey { get; set; }
+        public static Keys ExampleModifierKey { get; set; }
+        public static ControllerButtons ExampleButton { get; set; }
+        public static ControllerButtons ExampleModifierButton { get; set; }
+        public static Boolean ShowDebug { get; set; }
+
+        //Initialization of the plugin.
+        public static void Main()
+        {
+            Game.LogTrivial("Loading " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " settings...");
+            LoadValuesFromIniFile();
+
+            Game.LogTrivial(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " has been initialised.");
+
+            // Cinematic control fiber
+            GameFiber.StartNew(delegate
+            {
+                while (true)
+                {
+                    GameFiber.Yield();
+
+                    //Check for key press
+                    if (
+                            (Game.IsKeyDown(ExampleKey)
+                                && (Game.IsKeyDownRightNow(ExampleModifierKey)
+                                || ExampleModifierKey == Keys.None)
+                            )
+                            ||
+                            (Game.IsControllerButtonDown(ExampleButton)
+                                && (Game.IsControllerButtonDownRightNow(ExampleModifierButton)
+                                || ExampleModifierButton == ControllerButtons.None)
+                            )
+                        )
+                    {
+                        // Call the thing to do here!
+
+                        GameFiber.Sleep(1000);
+                    }
+                }
+            });
+        }
+
+        private static void DoCodeHere()
+        {
+            try
+            {
+                // Do your stuff here!
+            }
+            catch (Exception e)
+            {
+                ErrorLogger(e, "Activation", "Error during execution");
+            }
+        }
+
+        private static void LoadValuesFromIniFile()
+        {
+            InitializationFile ini = new InitializationFile(INIpath);
+            ini.Create();
+
+            try
+            {
+                //Keyboard ini
+
+                if (ini.DoesKeyExist("Keyboard", "ExampleKey")) { ExampleKey = ini.ReadEnum<Keys>("Keyboard", "ExampleKey", Keys.R); }
+                else
+                {
+                    ini.Write("Keyboard", "ExampleKey", "R");
+                    ExampleKey = Keys.R;
+                }
+
+                if (ini.DoesKeyExist("Keyboard", "ExampleModifierKey")) { ExampleModifierKey = ini.ReadEnum<Keys>("Keyboard", "ExampleModifierKey", Keys.ControlKey); }
+                else
+                {
+                    ini.Write("Keyboard", "ExampleModifierKey", "ControlKey");
+                    ExampleModifierKey = Keys.ControlKey;
+                }
+
+                // Controller ini
+
+                if (ini.DoesKeyExist("Controller", "ExampleButton")) { ExampleButton = ini.ReadEnum<ControllerButtons>("Controller", "ExampleButton", ControllerButtons.None); }
+                else
+                {
+                    ini.Write("Controller", "ExampleButton", "None");
+                    ExampleButton = ControllerButtons.None;
+                }
+
+                if (ini.DoesKeyExist("Controller", "ExampleModifierButton")) { ExampleModifierButton = ini.ReadEnum<ControllerButtons>("Controller", "ExampleModifierButton", ControllerButtons.None); }
+                else
+                {
+                    ini.Write("Controller", "ExampleModifierButton", "None");
+                    ExampleModifierButton = ControllerButtons.None;
+                }
+
+                // Other ini
+
+                if (ini.DoesKeyExist("Other", "ShowDebug")) { ShowDebug = ini.ReadBoolean("Other", "ShowDebug", false); }
+                else
+                {
+                    ini.Write("Other", "ShowDebug", "false");
+                    ShowDebug = false;
+                }
+
+                Game.LogTrivial("Settings initialisation complete.");
+            }
+            catch (Exception e)
+            {
+                ErrorLogger(e, "Initialisation", "Unable to read INI file.");
+            }
+        }
+
+        public static void Command_Debug(string text)
+        {
+            if (ShowDebug == true)
+            {
+                Game.DisplayNotification(text);
+            }
+        }
+
+        public static void ErrorLogger(Exception Err, String ErrMod, String ErrDesc)
+        {
+            Game.LogTrivial("--------------------------------------");
+            Game.LogTrivial("Error during " + ErrMod);
+            Game.LogTrivial("Decription: " + ErrDesc);
+            Game.LogTrivial(Err.ToString());
+            Game.DisplayNotification("~r~~h~" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + ":~h~~s~ Error during " + ErrMod + ". Please send logs.");
+        }
+    }
+}
---
+++ RPH_Delete_Vehicle/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using Rage.Attributes;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: Plugin("RPH_Template", Description = "Description_Here", PrefersSingleInstance = true, Author = "Waynieoaks")]
+[assembly: AssemblyTitle("RPH_Delete_Vehicle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RPH_Delete_Vehicle")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c916fcb-8530-4ca2-a7d7-b7b3221063f4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
